7211552230eff30fac36f436e841047684d057b6
TA <aaltopi@gmail.com>
2025-05-19 16:59:26 +0300

    Add Identification and Authentication Failures example
    Very simple as it's just turning off default Django settings,
    but difficult to really do it any other way. Although, I guess
    there's maybe some more fun approaches to adding this flaw that
    could be used instead.

--- app/flawedsite/settings.py
+++ app/flawedsite/settings.py
@@ -98,20 +98,37 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+# FLAW: Identification and Authentication Failures
+# ------------------------------------------------
+
+# PROPER VERSION
+# --------------
+
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+
+# PROPER VERSION
+# ==============
+
+
+# FLAWED VERSION
+# --------------
+# just straight up nothing
+
+# FLAWED VERSION
+# ==============
 
 
 # Internationalization
